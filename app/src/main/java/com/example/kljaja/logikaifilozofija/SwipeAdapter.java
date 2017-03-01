b86011e4442caa0aafb9bd20a3b3ddd638cb864f
kljaja998 <mladen98@gmail.com>
2017-03-01 18:47:06 +0100

    v0103171846

--- app/src/main/java/com/example/kljaja/logikaifilozofija/SwipeAdapter.java
+++ app/src/main/java/com/example/kljaja/logikaifilozofija/SwipeAdapter.java
@@ -42,77 +42,12 @@ public class SwipeAdapter extends FragmentStatePagerAdapter {
         }
         Bundle bundle = new Bundle();
 
+        int minPosition = SwipeMethods.getMinimumNumber();
 
-
-        int minPosition = 0;
-        if ((MainActivity.izborPredmeta == 1)&&(MainActivity.izborStrane == 2)){
-            minPosition = 45;
-        }
-        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 2)){
-            minPosition = 28;
-        }
-        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 3)){
-            minPosition = 57;
-        }
-        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 4)){
-            minPosition = 86;
-        }
-        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 6)){
-            minPosition = 57;
-        }
-        if ((MainActivity.izborPredmeta == 3)&&(MainActivity.izborStrane == 2)){
-            minPosition = 46;
-        }
-
-
-        byte a = 0;
+        int a;
 
         if(MainActivity.izborAktivnosti == 3){
-            switch (MainActivity.izborPredmeta){
-                case 1:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=45;
-                            break;
-                        case 2: a=40;
-                            break;
-                        case 5: a=85;
-                            break;
-                    }
-                    break;
-
-                case 2:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=28;
-                            break;
-                        case 2: a=29;
-                            break;
-                        case 3: a=29;
-                            break;
-                        case 4: a=27;
-                            break;
-                        case 5: a=57;
-                            break;
-                        case 6: a=56;
-                            break;
-                        case 7: a=113;
-                            break;
-                    }
-                    break;
-
-
-                case 3:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=46;
-                            break;
-                        case 2: a=42;
-                            break;
-                        case 5: a=88;
-                            break;
-                    }
-                    break;
-
-
-            }
+            a = SwipeMethods.getNumberForPage();
             maxPosition = a;
         }
 
@@ -154,51 +89,7 @@ public class SwipeAdapter extends FragmentStatePagerAdapter {
     public int getCount() {
         int a=1;
         if((MainActivity.izborAktivnosti==1)|| (MainActivity.izborAktivnosti ==2)){
-            switch (MainActivity.izborPredmeta){
-                case 1:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=45;
-                            break;
-                        case 2: a=40;
-                            break;
-                        case 5: a=85;
-                            break;
-                    }
-                    break;
-
-                case 2:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=28;
-                            break;
-                        case 2: a=29;
-                            break;
-                        case 3: a=29;
-                            break;
-                        case 4: a=27;
-                            break;
-                        case 5: a=57;
-                            break;
-                        case 6: a=56;
-                            break;
-                        case 7: a=113;
-                            break;
-                    }
-                    break;
-
-
-                case 3:
-                    switch (MainActivity.izborStrane){
-                        case 1: a=46;
-                            break;
-                        case 2: a=42;
-                            break;
-                        case 5: a=88;
-                            break;
-                    }
-                    break;
-
-
-            }
+            a = SwipeMethods.getNumberForPage();
         }
 
         if (MainActivity.izborAktivnosti == 2){
--- app/src/main/java/com/example/kljaja/logikaifilozofija/SwipeMethods.java
+++ app/src/main/java/com/example/kljaja/logikaifilozofija/SwipeMethods.java
@@ -1,8 +1,82 @@
 package com.example.kljaja.logikaifilozofija;
 
-/**
- * Created by kljaja on 01-Mar-17.
- */
 
-public class SwipeMethods {
+
+class SwipeMethods {
+    static int getNumberForPage() {
+        int a=1;
+        switch (MainActivity.izborPredmeta){
+            case 1:
+                switch (MainActivity.izborStrane){
+                    case 1: a=45;
+                        break;
+                    case 2: a=40;
+                        break;
+                    case 5: a=85;
+                        break;
+                }
+                break;
+
+            case 2:
+                switch (MainActivity.izborStrane){
+                    case 1: a=28;
+                        break;
+                    case 2: a=29;
+                        break;
+                    case 3: a=29;
+                        break;
+                    case 4: a=27;
+                        break;
+                    case 5: a=57;
+                        break;
+                    case 6: a=56;
+                        break;
+                    case 7: a=113;
+                        break;
+                }
+                break;
+
+
+            case 3:
+                switch (MainActivity.izborStrane){
+                    case 1: a=46;
+                        break;
+                    case 2: a=42;
+                        break;
+                    case 5: a=88;
+                        break;
+                }
+                break;
+
+
+        }
+
+        return a;
+    }
+    static int getMinimumNumber(){
+        int a = 0;
+        if ((MainActivity.izborPredmeta == 1)&&(MainActivity.izborStrane == 2)){
+            a = 45;
+        }
+
+        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 2)){
+            a = 28;
+        }
+        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 3)){
+            a = 57;
+        }
+        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 4)){
+            a = 86;
+        }
+        if ((MainActivity.izborPredmeta == 2)&&(MainActivity.izborStrane == 6)){
+            a = 57;
+        }
+        if ((MainActivity.izborPredmeta == 3)&&(MainActivity.izborStrane == 2)){
+            a = 46;
+        }
+        return a;
+    }
+
+
+
 }
